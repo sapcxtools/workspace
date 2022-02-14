633f1b6f8259657ec474ab59dd554fcf2da117fa
Stefan Bechtold <stefan.bechtold@arineo.com>
2022-02-14 01:34:42 +0100

    rename sapcxtemplate extension to sapcxreporting

--- src/tools/sapcx/commerce/reporting/constants/CxReportingConstants.java
+++ src/tools/sapcx/commerce/reporting/constants/CxReportingConstants.java
@@ -1,13 +1,13 @@
-package tools.sapcx.commerce.template.constants;
+package tools.sapcx.commerce.reporting.constants;
 
 /**
  * Global class for all constants. This class is required by the platform and
  * will not been used for any other purpose.
  */
-public final class CxTemplateConstants {
-	public static final String EXTENSIONNAME = "sapcxtemplate";
+public final class CxReportingConstants {
+	public static final String EXTENSIONNAME = "sapcxreporting";
 
-	private CxTemplateConstants() {
+	private CxReportingConstants() {
 		// empty to avoid instantiating this constant class
 	}
 }
