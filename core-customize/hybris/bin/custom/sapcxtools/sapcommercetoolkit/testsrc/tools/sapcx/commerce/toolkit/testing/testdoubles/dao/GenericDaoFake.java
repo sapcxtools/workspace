fcb69e9700f6877c4c77ebb2766cd54a0c6b6340
Stefan Bechtold <stefan.bechtold@me.com>
2022-03-26 15:06:57 +0100

    apply code formatter

--- core-customize/hybris/bin/custom/sapcxtools/sapcommercetoolkit/testsrc/tools/sapcx/commerce/toolkit/testing/testdoubles/dao/GenericDaoFake.java
+++ core-customize/hybris/bin/custom/sapcxtools/sapcommercetoolkit/testsrc/tools/sapcx/commerce/toolkit/testing/testdoubles/dao/GenericDaoFake.java
@@ -12,81 +12,81 @@ import de.hybris.platform.servicelayer.internal.dao.SortParameters;
 import tools.sapcx.commerce.toolkit.testing.itemmodel.InMemoryModelFactory;
 
 public class GenericDaoFake<T extends ItemModel> implements GenericDao<T> {
-    public static <T extends RuntimeException> GenericDao throwingException(Class<T> exceptionToThrow) {
-        return new GenericDaoFakeThrowingException<T>(exceptionToThrow);
-    }
+	public static <T extends RuntimeException> GenericDao throwingException(Class<T> exceptionToThrow) {
+		return new GenericDaoFakeThrowingException<T>(exceptionToThrow);
+	}
 
-    public static <T extends ItemModel> GenericDaoFake<T> empty() {
-        return new GenericDaoFake<T>(List.of());
-    }
+	public static <T extends ItemModel> GenericDaoFake<T> empty() {
+		return new GenericDaoFake<T>(List.of());
+	}
 
-    public static <T extends ItemModel> GenericDao<T> ofCount(int count, Class<T> modelClass) {
-        List<T> models = new ArrayList<>();
-        for (int i = 0; i < count; i++) {
-            models.add(InMemoryModelFactory.createTestableItemModel(modelClass));
-        }
-        return forModels(models);
-    }
+	public static <T extends ItemModel> GenericDao<T> ofCount(int count, Class<T> modelClass) {
+		List<T> models = new ArrayList<>();
+		for (int i = 0; i < count; i++) {
+			models.add(InMemoryModelFactory.createTestableItemModel(modelClass));
+		}
+		return forModels(models);
+	}
 
-    public static <T extends ItemModel> GenericDaoFake<T> forModel(T model) {
-        return new GenericDaoFake<T>(List.of(model));
-    }
+	public static <T extends ItemModel> GenericDaoFake<T> forModel(T model) {
+		return new GenericDaoFake<T>(List.of(model));
+	}
 
-    public static <T extends ItemModel> GenericDaoFake<T> forModels(List<T> models) {
-        return new GenericDaoFake<T>(models);
-    }
+	public static <T extends ItemModel> GenericDaoFake<T> forModels(List<T> models) {
+		return new GenericDaoFake<T>(models);
+	}
 
-    public int count;
-    public Map<String, ?> map;
+	public int count;
+	public Map<String, ?> map;
 
-    public SortParameters sortParameters;
+	public SortParameters sortParameters;
 
-    private List<T> models;
+	private List<T> models;
 
-    public GenericDaoFake(List<T> models) {
-        this.models = new ArrayList<>(models);
-    }
+	public GenericDaoFake(List<T> models) {
+		this.models = new ArrayList<>(models);
+	}
 
-    @Override
-    public List<T> find() {
-        return this.models;
-    }
+	@Override
+	public List<T> find() {
+		return this.models;
+	}
 
-    @Override
-    public List<T> find(Map<String, ?> map) {
-        this.map = map;
-        return models.stream().filter(model -> matchesCriteriaMap(map, model)).collect(Collectors.toList());
+	@Override
+	public List<T> find(Map<String, ?> map) {
+		this.map = map;
+		return models.stream().filter(model -> matchesCriteriaMap(map, model)).collect(Collectors.toList());
 
-    }
+	}
 
-    @Override
-    public List<T> find(SortParameters sortParameters) {
-        this.sortParameters = sortParameters;
-        return this.models;
-    }
+	@Override
+	public List<T> find(SortParameters sortParameters) {
+		this.sortParameters = sortParameters;
+		return this.models;
+	}
 
-    @Override
-    public List<T> find(Map<String, ?> map, SortParameters sortParameters) {
-        this.map = map;
-        this.sortParameters = sortParameters;
-        return models.stream().filter(model -> matchesCriteriaMap(map, model)).collect(Collectors.toList());
-    }
+	@Override
+	public List<T> find(Map<String, ?> map, SortParameters sortParameters) {
+		this.map = map;
+		this.sortParameters = sortParameters;
+		return models.stream().filter(model -> matchesCriteriaMap(map, model)).collect(Collectors.toList());
+	}
 
-    @Override
-    public List<T> find(Map<String, ?> map, SortParameters sortParameters, int count) {
-        this.map = map;
-        this.sortParameters = sortParameters;
-        this.count = count;
-        return models.stream().filter(model -> matchesCriteriaMap(map, model)).limit(count).collect(Collectors.toList());
+	@Override
+	public List<T> find(Map<String, ?> map, SortParameters sortParameters, int count) {
+		this.map = map;
+		this.sortParameters = sortParameters;
+		this.count = count;
+		return models.stream().filter(model -> matchesCriteriaMap(map, model)).limit(count).collect(Collectors.toList());
 
-    }
+	}
 
-    private boolean matchesCriteriaMap(Map<String,?> map, T model) {
-        for (String key : map.keySet()) {
-            if (!model.getProperty(key).equals(map.get(key))) {
-                return false;
-            }
-        }
-        return true;
-    }
+	private boolean matchesCriteriaMap(Map<String, ?> map, T model) {
+		for (String key : map.keySet()) {
+			if (!model.getProperty(key).equals(map.get(key))) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
