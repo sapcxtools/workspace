41bcc7fde2fbe6c6beb5c76876074ad40a017ffb
Stefan Bechtold <bechte@users.noreply.github.com>
2023-06-16 12:06:16 +0200

    format autogenerated constant class

--- core-customize/hybris/bin/custom/sapcxtools/sapcxsearch/src/tools/sapcx/commerce/search/constants/CxTemplateConstants.java
+++ core-customize/hybris/bin/custom/sapcxtools/sapcxsearch/src/tools/sapcx/commerce/search/constants/CxTemplateConstants.java
@@ -1,14 +1,11 @@
 package tools.sapcx.commerce.search.constants;
 
-@SuppressWarnings({"deprecation","squid:CallToDeprecatedMethod"})
-public class CxTemplateConstants extends GeneratedCxTemplateConstants
-{
+@SuppressWarnings({ "deprecation", "squid:CallToDeprecatedMethod" })
+public class CxTemplateConstants extends GeneratedCxTemplateConstants {
 	public static final String EXTENSIONNAME = "sapcxsearch";
-	
-	private CxTemplateConstants()
-	{
-		//empty
+
+	private CxTemplateConstants() {
+		// empty
 	}
-	
-	
+
 }
