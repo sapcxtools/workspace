fd8afa9d45952feb4f0eac46fd9b77c76a85ce87
Stefan Bechtold <bechte@users.noreply.github.com>
2023-12-08 11:50:16 +0100

    add session handling to simplify execution of code in local views

---
+++ core-customize/hybris/bin/custom/sapcxtools/sapcommercetoolkit/src/tools/sapcx/commerce/toolkit/session/LocalViewExecutor.java
@@ -0,0 +1,30 @@
+package tools.sapcx.commerce.toolkit.session;
+
+import static org.apache.commons.collections4.MapUtils.emptyIfNull;
+
+import java.util.Map;
+
+import de.hybris.platform.servicelayer.session.SessionExecutionBody;
+import de.hybris.platform.servicelayer.session.SessionService;
+
+/**
+ * This class is used to execute code in a local view. It basically wraps the execution of the method
+ * {@link SessionService#executeInLocalViewWithParams(Map, SessionExecutionBody)} by providing a more convenient
+ * approach using a lambda expression.
+ */
+public class LocalViewExecutor {
+	private SessionService sessionService;
+
+	public LocalViewExecutor(SessionService sessionService) {
+		this.sessionService = sessionService;
+	}
+
+	public <T> T with(Map<String, Object> parameter, LocalViewExecutionBody<T> code) {
+		return sessionService.executeInLocalViewWithParams(emptyIfNull(parameter), new SessionExecutionBody() {
+			@Override
+			public Object execute() {
+				return code.execute(sessionService);
+			}
+		});
+	}
+}
